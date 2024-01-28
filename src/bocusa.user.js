f831b5f4d0e3ef6a46718f9b956ae92ecab72b06
Jerry Chen <jerryc443@gmail.com>
2024-01-28 00:07:42 -0500

    u

--- src/bocusa.user.js
+++ src/bocusa.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Refresh BOCUSA balance
 // @author        jerryc05
-// @version       7
+// @version       9
 // @icon          https://www.bocusa.com/themes/custom/boc/slice/assets/images/favicon.png
 // @match         *://*/*
 // @run-at        document-body
@@ -9,6 +9,7 @@
 // @downloadURL   https://raw.githubusercontent.com/jerryc05/MyUserscripts/master/src/bocusa.user.js
 // @grant         GM_setValue
 // @grant         GM_getValue
+// @grant         GM_addValueChangeListener
 // @grant         GM_xmlhttpRequest
 // @grant         GM_deleteValue
 // ==/UserScript==
@@ -20,6 +21,7 @@
     ' ',
     '_'
   )
+  const GM_VALUE_RESP_STR_ID = GM_VALUE_ID + ' resp_str'
   const LOADING_STR = 'Loading BOCUSA balance ...'
 
   const elem = document.createElement('code')
@@ -30,13 +32,11 @@
   elem.style.zIndex = '99999999'
   elem.style.backgroundColor = 'rgba(0,0,0,0.5)'
   elem.style.color = 'white'
-  elem.style.padding = '.5em'
+  elem.style.padding = '.5rem'
+  elem.style.fontSize = '.8rem'
+  elem.style.lineHeight = '1rem'
+  elem.textContent = LOADING_STR
   document.body.append(elem)
-
-  // Check if the script is already running
-  if (GM_getValue(GM_VALUE_ID, 0) >= 3) {
-    return // Exit if already running
-  }
 
   // Set the singleton flag
   GM_setValue(GM_VALUE_ID, GM_getValue(GM_VALUE_ID, 0) + 1)
@@ -45,40 +45,58 @@
     GM_setValue(GM_VALUE_ID, GM_getValue(GM_VALUE_ID, 0) - 1)
   })
 
-  while (true) {
-    elem.textContent += '\nLoading BOCUSA balance ...'
-    const resp_str = await new Promise(r =>
-      GM_xmlhttpRequest({
-        method: 'POST',
-        url: 'https://ebanking.bocusa.com/newpbank/gw/acct/overview',
-        headers: {
-          accept: 'application/json, text/plain, */*',
-          'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
-          'sec-ch-ua':
-            '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-          'sec-ch-ua-mobile': '?0',
-          'sec-ch-ua-platform': '"Windows"',
-          'sec-fetch-dest': 'empty',
-          'sec-fetch-mode': 'cors',
-          'sec-fetch-site': 'same-origin',
-        },
-        onload: resp_ => {
-          console.dir(resp_)
-          r(resp_.response)
-        },
-      })
-    )
+  // Check if the script is already running
+  if (GM_getValue(GM_VALUE_ID, 0) <= 3) {
+    while (true) {
+      // set loading
+      GM_setValue(GM_VALUE_RESP_STR_ID, '')
 
-    const resp_json = JSON.parse(resp_str)
-    elem.textContent =
-      parseInt(resp_json._code) === 0
-        ? resp_json._data?.acctList
-            ?.map(
-              x => `${x.ACC}: $${parseFloat(x.BOOK_BAL.replaceAll(',', ''))}`
-            )
-            .join('\n') +
-        : resp_str
+      const resp_str = await new Promise(r =>
+        GM_xmlhttpRequest({
+          method: 'POST',
+          url: 'https://ebanking.bocusa.com/newpbank/gw/acct/overview',
+          headers: {
+            accept: 'application/json, text/plain, */*',
+            'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+            'sec-ch-ua':
+              '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+          },
+          onload: resp_ => {
+            console.dir(resp_)
+            r(resp_.response)
+          },
+        })
+      )
+      GM_setValue(GM_VALUE_RESP_STR_ID, resp_str)
 
-    await new Promise(r => setTimeout(r, 60_000))
+      await new Promise(r => setTimeout(r, 60_000))
+    }
   }
+
+  GM_addValueChangeListener(
+    GM_VALUE_RESP_STR_ID,
+    (_name, _old_value, resp_str) => {
+      if (!resp_str) {
+        // loading
+        elem.textContent += '\n' + LOADING_STR
+      } else {
+        // loaded
+        const resp_json = JSON.parse(resp_str)
+        elem.textContent =
+          parseInt(resp_json._code) === 0
+            ? resp_json._data?.acctList
+                ?.map(
+                  x =>
+                    `${x.ACC}: $${parseFloat(x.BOOK_BAL.replaceAll(',', ''))}`
+                )
+                .join('\n')
+            : resp_str
+      }
+    }
+  )
 })()
